5d59a69e0874d8bafc94045e9b231409479d7efd
pranay <pranay.s.chauhan@gmail.com>
2020-05-27 15:51:38 +0530

    my commit in redux

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ console.log('In Index.js');
 //Redux is state management library for javascript apps in a predictable fashion.
 
 //The following three are important concept in react as follows-
+/*
+CAKE SHOP SCENARIO =======  REDUX=============PURPOSE============================
+
+
+*/
